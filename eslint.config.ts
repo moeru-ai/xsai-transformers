4025872b7a14f715656b763398ea8b32bd19a65b
Neko Ayaka <neko@ayaka.moe>
2025-05-01 16:56:53 +0800

    fix: lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,14 @@
 import { GLOB_MARKDOWN_CODE, GLOB_TESTS } from '@antfu/eslint-config'
 import { defineConfig } from '@importantimport/eslint-config'
 
+// @ts-expect-error - fix this
 export default defineConfig([{
   rules: {
     '@masknet/no-default-error': 'off',
     '@masknet/no-then': 'off',
     'sonarjs/todo-tag': 'warn',
   },
+  vue: true,
 }, {
   files: [...GLOB_TESTS, GLOB_MARKDOWN_CODE],
   rules: {
